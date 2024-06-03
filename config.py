65f7bbcc7e44b9c22815e606c41ff21ed0481832
johboe <johannes.kroev@gmail.com>
2024-06-03 20:14:57 +0100

    Delete all old scripts

--- config.py
+++
@@ -1,9 +0,0 @@
-# URL zur Excel-Datei, die du öffnen möchtest
-excel_url = 'EXCEL_URL'
-
-# Benutzerdaten für die Anmeldung bei Excel Online
-username = 'MAILADRESSE'
-password = 'PASSWORT'
-
-# Maximaler Timeout, bis die Website geladen werden muss
-max_timeout = 240
